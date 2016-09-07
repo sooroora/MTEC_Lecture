2359a213581a9d82c4895d7f224cc333d4c57621
sooroora <sura6856@naver.com>
2016-09-07 09:40:11 +0900

    160907
    - git ignore

---
+++ Assets/Lecture/Scripts/Editior/MyComponentEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+
+[CustomEditor(typeof(MyComponent))]
+public class MyComponentEditor : Editor
+{
+    void OnEnable()
+    {
+
+    }
+
+
+    public override void OnInspectorGUI()
+    {
+       // base.OnInspectorGUI();
+
+        MyComponent myComponent = (MyComponent)target;
+        myComponent.intVariable = EditorGUILayout.IntField("int variable", myComponent.intVariable);
+        myComponent.floatVariable = EditorGUILayout.Slider("float variable", myComponent.floatVariable, 0f, 100f);
+        myComponent.IntVar = EditorGUILayout.IntField("int Var", myComponent.IntVar);
+
+        if(GUILayout.Button("Do Something") == true)
+        {
+            myComponent.DoSomething();
+        }
+        
+    }
+
+}
