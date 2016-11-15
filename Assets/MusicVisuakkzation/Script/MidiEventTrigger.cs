51a80eb8df478021b5ab75755094c4034d445a12
sooroora <sura6856@naver.com>
2016-11-15 22:58:51 +0900

    Day8

---
+++ Assets/MusicVisuakkzation/Script/MidiEventTrigger.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+
+
+public class MidiEventTrigger : MonoBehaviour
+{
+
+    public bool[] instrumentFilter = new bool[129];
+
+    public bool[] noteFilter = new bool[128];
+
+
+
+    private bool _noteOn = false;
+
+
+
+    public void Play()
+    {
+
+        _noteOn = false;
+
+        OnPlay();
+
+    }
+
+
+
+    public void Pause()
+    {
+
+        OnPause();
+
+    }
+
+
+
+    public void Resume()
+    {
+
+        OnResume();
+
+    }
+
+
+
+    public void Stop()
+    {
+
+        OnStop();
+
+    }
+
+
+
+    public void NoteOn(int instrument, int noteNumber)
+    {
+
+        if (_noteOn == false)
+        {
+
+            _noteOn = true;
+
+
+
+            if (instrumentFilter[instrument] == true && noteFilter[noteNumber] == true)
+            {
+
+                Debug.Log("NoteOn");
+
+                OnNoteOn();
+
+            }
+
+        }
+
+    }
+
+
+
+    public void NoteOff(int instrument, int noteNumber)
+    {
+
+        _noteOn = false;
+
+
+
+        if (instrumentFilter[instrument] == true && noteFilter[noteNumber] == true)
+        {
+
+            Debug.Log("NoteOff");
+
+            OnNoteOff();
+
+        }
+
+    }
+
+
+
+    protected virtual void OnPlay()
+    {
+
+    }
+
+
+
+    protected virtual void OnPause()
+    {
+
+    }
+
+
+
+    protected virtual void OnResume()
+    {
+
+    }
+
+
+
+    protected virtual void OnStop()
+    {
+
+    }
+
+
+
+    protected virtual void OnNoteOn()
+    {
+
+    }
+
+
+
+    protected virtual void OnNoteOff()
+    {
+
+    }
+
+}
--- Assets/MusicVisuakkzation/Script/MidiPlayer.cs
+++ Assets/MusicVisuakkzation/Script/MidiPlayer.cs
@@ -1,36 +1,51 @@
 ﻿using UnityEngine;
+
 using System.Collections;
+
+
+
 
 
 public class MidiPlayer : MonoBehaviour
 {
 
     public MidiAsset midi;
+
     public AudioClip music;
+
     public AudioSource audioSource;
+
     public float playDelayTime = 0f;
+
     public float playSpeed = 1f;
 
 
 
     private bool _isPlaying = false;
+
     private float _playTime = 0f;
+
     private float _totalTime = 0f;
 
     private float _audioDelayTime;
+
     private float _midiDelayTime;
 
     private bool _audioStarted = false;
 
-    // Use this for initialization
+    private MidiTrack[] _tracks;
 
-
-    MidiTrack[] _tracks;
     private int[] _noteIndex;
 
     private float _pulseTime;
 
+    private MidiEventTrigger[] _triggers;
 
+
+
+
+
+    // Use this for initialization
 
     void Start()
     {
@@ -48,61 +63,95 @@ public class MidiPlayer : MonoBehaviour
 
         if (_isPlaying == true)
         {
-            if(_audioDelayTime >0)
-            {
+
+            if (_audioDelayTime > 0f)
+
                 _audioDelayTime -= Time.deltaTime;
-            }
+
             else
             {
-                if (!_audioStarted)
+
+                if (_audioStarted == false)
                 {
+
                     _audioStarted = true;
+
                     if (audioSource != null)
+
                         audioSource.Play();
+
                 }
 
             }
 
 
-            if(_midiDelayTime >0f)
-            {
+
+            if (_midiDelayTime > 0f)
+
                 _midiDelayTime -= Time.deltaTime;
-            }
+
             else
             {
+
                 _playTime += (Time.deltaTime * playSpeed);
 
-                for(int i= 0;i<_tracks.Length;++i)
+                for (int i = 0; i < _tracks.Length; i++)
                 {
-                    for(int j = _noteIndex[i] ; j< _tracks[i].Number ;++j)
+
+                    int noteCount = _tracks[i].Notes.Count;
+
+                    for (int j = _noteIndex[i]; j < noteCount; j++)
                     {
+
                         MidiNote note = _tracks[i].Notes[j];
+
                         float sTime = note.StartTime * _pulseTime;
+
                         float eTime = note.EndTime * _pulseTime;
 
 
+
                         if (_playTime < sTime)
-                        {
+
                             break;
+
+
+
+                        // Midi Event Trigger Call
+
+                        foreach (MidiEventTrigger trigger in _triggers)
+                        {
+
+                            trigger.NoteOn(_tracks[i].Instrument, note.Number);
+
                         }
 
-                        Debug.Log(note.Number);
+
 
                         if (_playTime > eTime)
                         {
+
                             _noteIndex[i] = j + 1;
+
+
+
+                            // Midi Event Trigger Call
+
+                            foreach (MidiEventTrigger trigger in _triggers)
+                            {
+
+                                trigger.NoteOff(_tracks[i].Instrument, note.Number);
+
+                            }
+
                         }
+
                     }
 
-
-                   
                 }
 
-
-
-
             }
-            
+
         }
 
     }
@@ -113,49 +162,79 @@ public class MidiPlayer : MonoBehaviour
     {
 
         if (midi == null)
+
             return;
 
-        //여기
-        if(music != null && audioSource !=null )
-        {
-            audioSource.clip = music;
 
-        }
+
+        if (music != null && audioSource != null)
+
+            audioSource.clip = music;
 
 
 
         _isPlaying = true;
+
         _playTime = 0f;
+
         _totalTime = midi.totalTime;
+
         _pulseTime = midi.pulseTime;
 
-        if(playDelayTime == 0f)
+        if (playDelayTime == 0f)
         {
+
             _audioDelayTime = 0f;
-            _midiDelayTime=0;
-        }
-        else if(playDelayTime >0f)
-        {
-            _audioDelayTime = playDelayTime;
+
             _midiDelayTime = 0f;
+
         }
+
+        else if (playDelayTime > 0f)
+        {
+
+            _audioDelayTime = playDelayTime;
+
+            _midiDelayTime = 0f;
+
+        }
+
         else
         {
 
             _audioDelayTime = 0f;
-            _midiDelayTime = playDelayTime;
+
+            _midiDelayTime = -playDelayTime;
+
         }
 
-
         _audioStarted = false;
+
         _tracks = midi.tracks;
 
         _noteIndex = new int[_tracks.Length];
 
-        for(int i =0;i<_noteIndex.Length;++i)
-        {
+        for (int i = 0; i < _noteIndex.Length; i++)
+
             _noteIndex[i] = 0;
+
+
+
+        // Find Midi Event Trigger
+
+        _triggers = GameObject.FindObjectsOfType<MidiEventTrigger>();
+
+
+
+        // Midi Event Trigger Call
+
+        foreach (MidiEventTrigger trigger in _triggers)
+        {
+
+            trigger.Play();
+
         }
+
     }
 
 
@@ -165,8 +244,22 @@ public class MidiPlayer : MonoBehaviour
 
         _isPlaying = false;
 
+
+
         if (audioSource != null)
+
             audioSource.Pause();
+
+
+
+        // Midi Event Trigger Call
+
+        foreach (MidiEventTrigger trigger in _triggers)
+        {
+
+            trigger.Pause();
+
+        }
 
     }
 
@@ -174,10 +267,25 @@ public class MidiPlayer : MonoBehaviour
 
     public void Resume()
     {
+
         _isPlaying = true;
 
+
+
         if (audioSource != null)
+
             audioSource.UnPause();
+
+
+
+        // Midi Event Trigger Call
+
+        foreach (MidiEventTrigger trigger in _triggers)
+        {
+
+            trigger.Resume();
+
+        }
 
     }
 
@@ -185,11 +293,29 @@ public class MidiPlayer : MonoBehaviour
 
     public void Stop()
     {
+
         _isPlaying = false;
+
         _playTime = 0f;
 
+        _audioStarted = false;
+
+
+
         if (audioSource != null)
+
             audioSource.Stop();
+
+
+
+        // Midi Event Trigger Call
+
+        foreach (MidiEventTrigger trigger in _triggers)
+        {
+
+            trigger.Stop();
+
+        }
 
     }
 
